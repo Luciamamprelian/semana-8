a745f227ca8745f1c54e341ea594498909a8b600
luciamamprelian <luciamamprelian@gmail.com>
2024-08-31 16:32:27 -0300

    Update script.js

--- Ejercicio fetch y json/js/script.js
+++ Ejercicio fetch y json/js/script.js
@@ -1,29 +1,32 @@
-const DATA_URL = "json/data.json";
+// En este archivo no utilizamos el evento "DOMContentLoaded", ya que se colocó el atributo "defer" en la importación del script,
+// que nos soluciona el problema de los elementos no cargados del DOM. Más info => https://www.w3schools.com/tags/att_script_defer.asp
 
-const container = document.getElementById("container");
+const DATA_URL = "json/data.json"; // URL que contiene los datos que queremos mostrar
 
+const container = document.getElementById("container"); // "Traemos" utilizando el DOM el div de id "container" para colocar la información en él
+
+/**
+ * Función que recibe por parámetro un array con los datos que se mostrarán en el DOM
+ * Los datos se mostrarán dentro del div de id "container" y por cada ítem se está creando un nuevo párrafo donde se
+ * imprime el campo "name" y el campo "lastname" separados por un espacio
+ */
 function showData(dataArray) {
-  container.innerHTML = "Estudiantes encontrados:"
-  let contador = 1;
-  
+  // El for itera sobre los elementos del array
   for (const item of dataArray) {
-    container.innerHTML += `<p> ${contador}-${e.name} ${e.lastname} </p>`;
-  contador++;
+    // En la siguiente línea se utilizan "backticks" para armar el String. Más info => https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Template_literals
+    container.innerHTML += `<p> ${item.name} ${item.lastname} </p>`; // Se concatena cada párrafo de la manera que queremos mostrarlo al innerHTML del contenedor
   }
 }
 
-function GetEstudiantes() {
+// Escribe el código necesario para realizar el fetch al archivo con los datos y mostrar los estudiantes con la función showData
+function MostrarEstudiantes() {
   fetch(DATA_URL, {
     method: "GET"
   })
-  .then(function(response) {
-      return response.json();
-  })
-  .then(function(data) {
+  .then(response => response.json())
+  .then(data => {
     showData(data.students);
   })
-  .catch(function(error) {
-    console.error('Ocurrio un error', error);
-  });
+  .catch(error => console.error ('Error al cargar el archivo JSON', error));
 }
-GetEstudiantes();
+MostrarEstudiantes();
